e07ba1b1f3ab0157f2dde740c2c0244b90d157fb
Gábor DEÁK JAHN <deakjahn@gmail.com>
2020-06-05 14:43:15 +0200

    Add files via upload

--- example.dart
+++ example.dart
@@ -3,13 +3,12 @@
  * All rights reserved.
  */
 
-```dart
 final worker = BackgroundWorker();
 int counter = 0;
 
 void main() {
   // Start the worker/isolate at the `entryPoint` function.
-  isolates.spawn<int>(entryPoint,
+  worker.spawn<int>(entryPoint,
     name: "counter",
     // Executed every time data is received from the spawned worker/isolate.
     onReceive: setCounter,
@@ -24,21 +23,15 @@ void setCounter(int count) {
   print("Counter is now $counter");
   
   // We will no longer be needing the worker/isolate, let's dispose of it.
-  isolates.kill("counter");
+  worker.kill("counter");
 }
 
 // This function happens in the worker/isolate.
-void entryPoint(Map<String, dynamic> context) {
-  // Calling initialize from the entry point with the context is
-  // required if communication is desired. It returns a messenger which
-  // allows listening and sending information to the main worker/isolate.
-  final messenger = HandledIsolate.initialize(context);
-
+void entryPoint(String name) {
   // Triggered every time data is received from the main isolate.
-  messenger.listen((count) {
+  worker.listen((count) {
     // Add one to the count and send the new value back to the main
     // isolate.
-    messenger.send(++count);
-  });
+    worker.send(name, ++count);
+  }, name: name);
 }
-```
