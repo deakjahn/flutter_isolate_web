062c4b271b87873a1db95d77653d211dad373286
Gábor DEÁK JAHN <deakjahn@gmail.com>
2020-06-05 13:37:29 +0200

    Add files via upload

---
+++ interface.dart
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2020 DEÁK JAHN Gábor.
+ * All rights reserved.
+ */
+
+library worker;
+
+import 'package:flutter/foundation.dart';
+
+import 'worker.dart' //
+    if (dart.library.io) 'worker_io.dart'
+    if (dart.library.html) 'worker_web.dart';
+
+abstract class BackgroundWorker {
+  factory BackgroundWorker() => getWorker();
+
+  List<String> get names => [];
+
+  void spawn<T>(void Function(String) mainFunction, {@required String name, void Function() onInitialized, void Function(T message) onReceive});
+
+  void send(String name, dynamic message);
+
+  void listen(void Function(dynamic message) onData, {@required String name, void Function() onError, void Function() onDone, bool cancelOnError});
+
+  void kill(String name);
+}
